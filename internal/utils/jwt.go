36c9161f8ffb24b82b434d8e84f0a8b5666424fd
zuramai <saugi.dev@gmail.com>
2024-04-23 11:34:11 +0700

    feat: add expires in jwt

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -1,15 +1,24 @@
 package utils
 
-import "github.com/golang-jwt/jwt"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
 
 type JWTClaims struct {
 	jwt.StandardClaims
 	Email string `json:"email"`
 }
 
+const LOGIN_EXPIRATION_DURATION = time.Duration(1) * time.Hour
+
 func GenerateToken(email string, secret string) (string, error) {
 	claims := JWTClaims{
 		Email: email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(LOGIN_EXPIRATION_DURATION).Unix(),
+		},
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
